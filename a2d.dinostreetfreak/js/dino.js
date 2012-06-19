028c895fb0e2a041c7f888c427b3f650034f53fa
Armen <armen138@gmail.com>
2012-06-19 00:10:06 -0400

    15 minutes: ascii level loader.

--- a2d.dinostreetfreak/js/dino.js
+++ a2d.dinostreetfreak/js/dino.js
@@ -1,7 +1,42 @@
 var game = {
-	init: function() {
-		a2d.root.push(new a2d.Tile(a2d.resources["dino"], {position: new a2d.Position(100, 100)}));
-		a2d.root[0].frameLoop(new a2d.Vector(0, 2));
+	init: function() {	
+		game.get("data/level.dst?" + Math.random(), function(data) {
+			var lines = data.split("\n");
+			var level = {};
+			level.gridSize = [lines[lines.length -1].length, lines.length];
+			level.tileSize = [64, 64];
+			level.tileSet = "tiles";
+			level.tiles = [];
+			for(var x = 0; x < level.gridSize[0]; x++) {
+				for(var y = 0; y < level.gridSize[1]; y++) {
+					console.log(lines[y][x]);
+					if(lines[y].length > x) {
+						level.tiles.push(lines[y][x] === "-" ? 0 : -1);	
+					} else {
+						level.tiles.push(-1);
+					}					
+				}
+			}
+			for(var x = 0; x < level.gridSize[0]; x++) {
+				for(var y = 0; y < level.gridSize[1]; y++) {
+					level.tiles.push(-1);
+				}
+			}
+			game.level = new a2d.TileGrid(level);
+			a2d.root.push(game.level);
+			a2d.root.push(new a2d.Tile(a2d.resources["dino"], {position: new a2d.Position(100, 100)}));
+			console.log(game.level.getTiles());
+		})
+	},
+	get: function(datafile, cb) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", datafile);
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState === 4) {
+				cb(xhr.responseText);
+			}
+		};
+		xhr.send(null);
 	}
 };
 
@@ -10,5 +45,6 @@ window.onload = function() {
 	a2d.on("load", function() {
 		game.init();
 	});
-	a2d.load({"dino" : "images/dinosaur.png"});
+	a2d.load({	"dino" : "images/dinosaur.png",
+				"tiles": "images/tiles.png" });
 }
